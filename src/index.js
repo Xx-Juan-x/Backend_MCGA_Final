ee641234b8e355a0f255531601cd2adb04a54463
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-12-06 18:49:41 -0300

    Instalación de dependencias con archivo .env y prueba de conexión con la base de datos

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+//GET: para hacer ping al servidor y que devuelva 'OK' en caso que el server y la BD estén levantadas
+app.get('/',(req, res)=>{
+    res.send(200, "OK");
+})
+
+mongoose.connect(process.env.DB_CONNECT)
+.then(
+    app.listen(()=>{
+        console.log('DB OK');
+        app.listen(process.env.PORT, () => console.log('Server OK'))
+    })
+)
+.catch((error)=> console.log("DB Failed" + error))
